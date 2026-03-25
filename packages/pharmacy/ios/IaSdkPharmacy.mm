d97762ce09a1d11681d829fe29644d7d6fbf9853
Matan Ljubas <mljubas@4ofthem.eu>
2026-03-25 13:44:41 +0100

    Bugfixes

--- packages/pharmacy/ios/IaSdkPharmacy.mm
+++ packages/pharmacy/ios/IaSdkPharmacy.mm
@@ -15,6 +15,12 @@
   [[IaSdkPharmacyImpl shared] setPharmacyIdIOS:pharmacyId];
 }
 
+- (void)getPharmacyIdIOS:(RCTResponseSenderBlock)completion {
+  [[IaSdkPharmacyImpl shared] getPharmacyIdIOS:^(NSString *result) {
+    completion(@[ result ?: [NSNull null] ]);
+  }];
+}
+
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params {
   return std::make_shared<facebook::react::NativeIaSdkPharmacySpecJSI>(params);
